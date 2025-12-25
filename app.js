07eff05069f046dd31a8fd1ce410dccab0ac416c
Mdhussaindev <mdhussain.dev00@gmail.com>
2025-12-25 21:00:08 +0500

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Q1
 
 var user_integer = prompt("Enter a positive integer \n Example 1.4 0.3 or 4.6 ");
-// var user_integer = 3.5; //for direct value assign
+var user_integer = 3.5; //for direct value assign
 console.log("Number : "+user_integer);
 
 var round_integer = Math.round(user_integer)
@@ -14,3 +14,55 @@ console.log("Floor value : "+floor_integer);
 
 var ceil_integer = Math.ceil(user_integer)
 console.log("Ceil value : "+ceil_integer);
+
+// Q2
+
+var user_integer = prompt("Enter a positive integer \n Example 1.4 0.3 or 4.6 ");
+var user_integer = -2.673; //for direct value assign
+console.log("Number : "+user_integer);
+
+var round_integer = Math.round(user_integer)
+console.log("Round off value : "+round_integer);
+
+var floor_integer  = Math.floor(user_integer)
+console.log("Floor value : "+floor_integer);
+
+var ceil_integer = Math.ceil(user_integer)
+console.log("Ceil value : "+ceil_integer);
+
+// Q3
+
+var number = prompt("Enter a Nagative to Value to convert to positive value \n Example : -2 -44 or -4")
+console.log("Input : "+number);
+var conv_num = Math.abs(number);
+console.log("Output : "+conv_num)
+
+// Q4
+
+var dice_limit = 6;
+
+var random_dice = Math.random();
+
+var final_dice = (random_dice*dice_limit) + 1;
+console.log(final_dice)
+var floor_dice = Math.floor(final_dice);
+console.log(floor_dice)
+
+// Q5
+
+var coin_sides = 2;
+var random_side = Math.floor(Math.random()*coin_sides)+1;
+
+// -----------A-------------
+console.log(random_side === 1 ? "Head" : "Tails")
+
+// -----------B-------------
+if(random_side === 1)
+{
+    console.log("Random coin value: Heads" )
+}
+else
+{
+    console.log("Random coin value: Tails" )
+}
+
